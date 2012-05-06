691bc1b94356df5c5ced7afd653f3ae02ea99f25
Harri Salokorpi <hsalokor@iki.fi>
2012-05-07 01:40:31 +0300

    Very simple socket client for playing with conduits
    Echoes string to socket but does currently block

--- src/Network/Socketeer/Client/main.hs
+++ src/Network/Socketeer/Client/main.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main = putStrLn "Hello, World!"
+import Data.Conduit
+import Data.Conduit.List (sourceList)
+import Data.Conduit.Text (utf8, encode)
+import Data.Conduit.Network
+import Data.Conduit.Binary (sinkHandle)
+import System.IO (stdout)
+
+send :: Application IO
+send src sink = do
+    sourceList ["Foobar"] $= encode utf8 $$ sink
+    src $$ sinkHandle stdout
+
+main :: IO ()
+main = runTCPClient (ClientSettings 3001 "127.0.0.1") send
