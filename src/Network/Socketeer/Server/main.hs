35f15403f9d3057c8933884e811c887b849cd76f
Harri Salokorpi <hsalokor@iki.fi>
2012-05-06 23:29:49 +0300

    Implement simple echo server based on example

--- src/Network/Socketeer/Server/main.hs
+++ src/Network/Socketeer/Server/main.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main = putStrLn "Hello, World!"
+import Data.Conduit
+import Data.Conduit.Network
+
+echo :: Application IO
+echo src sink = src $$ sink
+
+main :: IO ()
+main = runTCPServer (ServerSettings 3001 "127.0.0.1") echo
